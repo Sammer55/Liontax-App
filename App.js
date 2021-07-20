bda7f27acba13cb766c4eab46e7ad446c59c9f84
Sammer55 <sammerduarte@hotmail.com>
2021-07-20 12:18:35 -0300

    Added Provider

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from './src/provider'
+import AppRoutes from './src/routes/stack.routes';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Provider>
+        <StatusBar barStyle='light-content' hidden={false} backgroundColor="#FF891D" />
+        <AppRoutes />
+      </Provider>
+    </NavigationContainer>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
