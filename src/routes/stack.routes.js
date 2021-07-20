cc55df40bfe6bdd8219af70461267348ad5901a2
Sammer55 <sammerduarte@hotmail.com>
2021-07-20 12:14:14 -0300

    Added stack routes

---
+++ src/routes/stack.routes.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack'
+import Search from '../pages/search';
+import Create from '../pages/create';
+import Edit from '../pages/edit';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const AppRoutes = () => {
+
+    function TabBarIcon({ name }) {
+        return <Ionicons size={30} color="#000" name={name} />;
+    }
+
+    return (
+        <Tab.Navigator tabBarOptions={{ activeBackgroundColor: "#FF990D" }}>
+            <Tab.Screen options={{ tabBarIcon: () => <TabBarIcon name="person-add-sharp" />, tabBarLabel: () => { return null }, }} name='Create' component={Create} />
+            <Tab.Screen options={{ tabBarIcon: () => <TabBarIcon name="md-list-sharp" />, tabBarLabel: () => { return null } }} name='Search' component={Search} />
+        </Tab.Navigator>
+    )
+}
+
+export default AppRoutes;
