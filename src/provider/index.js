cad04ec483009b1862e06bbcb847e6a06f846537
Sammer55 <sammerduarte@hotmail.com>
2021-07-20 12:15:06 -0300

    Added Provider Context

---
+++ src/provider/index.js
@@ -0,0 +1,22 @@
+import React, { createContext, useState } from 'react'
+
+export const Context = createContext({});
+
+export const Provider = (props) => {
+
+    const [name, setName] = useState({ name: '' })
+    const [cpf, setCpf] = useState({ name: '' })
+    const [email, setEmail] = useState({ email: email })
+    const [tel, setTel] = useState({ name: '' })
+
+    return (
+        <Context.Provider value={{ 
+            name, setName, 
+            cpf, setCpf, 
+            email, setEmail,
+            tel, setTel,
+            }}>
+            { props.children }
+        </Context.Provider>
+    )
+}
