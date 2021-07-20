0924e5881fa6473f00d5d4ad0390b31c321ebfdc
Sammer55 <sammerduarte@hotmail.com>
2021-07-20 13:03:55 -0300

    Removed Textformat

--- src/components/TextFormat/formatTel/index.js
+++
@@ -1,9 +0,0 @@
-export const formatTel = (tel) => {
-    const telToString = tel.toString()
-    const DDD = telToString.substr(1, 2)
-    const firstsNumbers = telToString.substr(5, 5)
-    const lastNumbers = telToString.substr(11, 4)
-    const telFormated = '+55' + DDD + firstsNumbers + lastNumbers
-    return telFormated;
-
-}
